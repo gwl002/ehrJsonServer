097d62d07f3e577b11e9adf97a3b5960968354e0
devingong <devingong@ideaculture.com>
2019-02-22 10:06:54 +0800

    change port for oles

--- oles/server.js
+++ oles/server.js
@@ -3,7 +3,7 @@ const server = jsonServer.create();
 
 const middlewares = jsonServer.defaults();
 const router = jsonServer.router(require("./olesData.js")());
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8004;
 
 
 server.use(middlewares);
