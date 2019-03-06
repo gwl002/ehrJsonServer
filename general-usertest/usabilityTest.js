59b93e2452e78889cb729f3f4740bc854366c30e
devingong <devingong@ideaculture.com>
2019-03-06 17:38:11 +0800

    sss

--- general-usertest/usabilityTest.js
+++ general-usertest/usabilityTest.js
@@ -1,12 +1,18 @@
 module.exports = function() {
   return Object.assign({},
+    require('./hcr.json'),
+    require('./allergyadr.json'),
+    require('./dispensing.json'),
     require('./system.json'),
-	require('./allergyadr.json'),
-	require('./dispensing.json'),
+    require('./searchResult.json'),
     require('./hci.json'),
     require('./hcp.json'),
     require('./pppdoctor.json'),
+    require('./ppphci.json'),
+    require('./ehrdoctor.json'),
+    require('./db.json'),
     require('./basicSearch.json'),
-    require('./ehrdoctor.json')
+    require('./advSearch.json'),
+    require('./notiHistory.json'),
     );
 };
